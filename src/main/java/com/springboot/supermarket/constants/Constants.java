1255bda6ae66583a10f5bef2a1ec30e553053911
Gokul <gokul@gokul.com>
2020-03-05 12:32:54 +0530

    Constants for Login Messages added.

---
+++ src/main/java/com/springboot/supermarket/constants/Constants.java
@@ -0,0 +1,8 @@
+package com.springboot.supermarket.constants;
+
+public class Constants {
+
+public static final String LOGIN_SUCCESS="User Login is successful";
+public static final String LOGIN_FAIL="Login Failed";
+public static final String AUTH_KEY="auth";
+}
