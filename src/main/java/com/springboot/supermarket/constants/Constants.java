9639eddb4e827976ebd1c70228940edb51f86de3
Gokul <gokul@gokul.com>
2020-03-05 12:33:44 +0530

    Constants for Login added.

--- src/main/java/com/springboot/supermarket/constants/Constants.java
+++ src/main/java/com/springboot/supermarket/constants/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
 public static final String LOGIN_SUCCESS="User Login is successful";
 public static final String LOGIN_FAIL="Login Failed";
 public static final String AUTH_KEY="auth";
+
 }
