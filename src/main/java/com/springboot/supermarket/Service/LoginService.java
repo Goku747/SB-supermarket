320e274308beec457c6198ba4e8764f9c416b132
Goku <Goku@DarkDragon>
2020-03-08 19:53:51 +0530

    Redis dependenci added

---
+++ src/main/java/com/springboot/supermarket/Service/LoginService.java
@@ -0,0 +1,18 @@
+package com.springboot.supermarket.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springboot.supermarket.Repository.UserInterface;
+
+@Service
+public class LoginService {
+	@Autowired
+	UserInterface userinterface;
+	
+	@SuppressWarnings("rawtypes")
+	@Autowired
+	
+	
+
+}
---
+++ src/main/java/com/springboot/supermarket/User/UserLogin.java
@@ -0,0 +1,34 @@
+package com.springboot.supermarket.User;
+
+public class UserLogin {
+Long id;
+String username;
+String authToken, status;
+
+public String getAuthToken() {
+	return authToken;
+}
+public void setAuthToken(String authToken) {
+	this.authToken = authToken;
+}
+public Long getId() {
+	return id;
+}
+public void setId(Long id) {
+	this.id = id;
+}
+public String getUsername() {
+	return username;
+}
+public void setUsername(String username) {
+	this.username = username;
+}
+
+public String getStatus() {
+	return status;
+}
+public void setStatus(String status) {
+	this.status = status;
+}
+
+}
